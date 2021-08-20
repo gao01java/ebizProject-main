f383ec646d348da0aa117e80372c65e8e0c01928
gao01java <60835579+gao01java@users.noreply.github.com>
2021-08-20 21:20:37 +0900

    Update

--- Project1/app/src/main/java/com/example/project1/accountActivity.java
+++ Project1/app/src/main/java/com/example/project1/accountActivity.java
@@ -1,13 +1,25 @@
 package com.example.project1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class accountActivity extends AppCompatActivity {
+
+
+
+    ImageButton backIb;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        backIb=(ImageButton)findViewById(R.id.back1);
+        backIb.setOnClickListener(v -> {
+            Intent intent=new Intent(accountActivity.this,MainActivity.class);
+            startActivity(intent);
+        });
 
 
 
